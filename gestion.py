6cfc29e48d2fef5fa588a97877f03a50c50f7638
diana01010 <djacanamejoy@unal.edu.co>
2024-12-12 08:43:37 -0500

    2

--- gestion.py
+++ gestion.py
@@ -1,3 +1,5 @@
+# gestion.py
+
 class Zoologico:
     def __init__(self, nombre=None, ubicacion=None):
         self.nombre = nombre
@@ -28,3 +30,32 @@ class Zoologico:
 
     def get_zonas(self):
         return self.zonas
+
+
+class Zona:
+    def __init__(self, nombre=None, zoo=None):
+        self.nombre = nombre
+        self.zoo = zoo
+        self.animales = []
+
+    def agregar_animales(self, animal):
+        self.animales.append(animal)
+
+    def cantidad_animales(self):
+        return len(self.animales)
+
+    # Métodos getters y setters
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def get_zoo(self):
+        return self.zoo
+
+    def set_zoo(self, zoo):
+        self.zoo = zoo
+
+    def get_animales(self):
+        return self.animales
