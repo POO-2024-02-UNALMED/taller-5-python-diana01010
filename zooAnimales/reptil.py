f1c5bff5d06d22ac720cbea86c374bb8ba44b5ff
diana01010 <djacanamejoy@unal.edu.co>
2024-12-12 23:40:32 -0500

    309

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -3,6 +3,8 @@ from zooAnimales.animal import Animal
 class Reptil(Animal):
     iguanas = 0
     serpientes = 0
+    "crearIguana"
+
 
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
         super().__init__(nombre, edad, habitat, genero)
