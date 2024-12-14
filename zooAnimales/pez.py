e1e5d0881dbb6cf21b46535c723a9b4ef9c6e2c0
diana01010 <djacanamejoy@unal.edu.co>
2024-12-14 18:41:31 -0500

    56

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -4,21 +4,20 @@ class Pez(Animal):
     bacalaos = 0
 
     def __init__(self, nombre, edad, habitat, genero, color=None, profundidad=None, aletas=0):
-        # Llamamos al constructor de la clase base Animal
-        super().__init__(nombre, edad, habitat, genero)
-        self.color = color  # Asignamos el color correctamente
+        super().__init__(nombre, edad, habitat, genero)  # Llamada al constructor de la clase base Animal
+        self.color = color  # Asigna el color correctamente
         self.profundidad = profundidad
-        self.aletas = aletas  # Asignamos el número de aletas correctamente
+        self.aletas = aletas  # Asigna el número de aletas
 
     @staticmethod
     def cantidadPeces():
         return Pez.salmones + Pez.bacalaos
 
     def getColorEscamas(self):
-        return self.color  # Este método debe devolver el color de las escamas correctamente
+        return self.color  # Devuelve el color de las escamas
 
     def getCantidadAletas(self):
-        return self.aletas  # Este método debe devolver el número de aletas correctamente
+        return self.aletas  # Devuelve el número de aletas
 
     # Métodos estáticos para crear salmones y bacalaos
     @staticmethod
@@ -32,6 +31,7 @@ class Pez(Animal):
         pez = Pez(nombre, edad, "mar profundo", genero, "plateado", 200, 3)
         Pez.bacalaos += 1
         return pez
+
 
 
 
