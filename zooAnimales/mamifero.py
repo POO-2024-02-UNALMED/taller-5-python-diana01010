89ea5193df8964cad824de9557620a50bf16ff45
diana01010 <djacanamejoy@unal.edu.co>
2024-12-15 12:07:58 -0500

    60

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -1,25 +1,44 @@
+from zooAnimales.animal import Animal
+
 class Mamifero(Animal):
+    # Contadores estáticos para caballos y leones
     caballos = 0
     leones = 0
 
-    def __init__(self, nombre, edad, habitat, genero):
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
         super().__init__(nombre, edad, habitat, genero)
+        self.pelaje = pelaje
+        self.patas = patas
 
     @staticmethod
     def cantidadMamiferos():
+        # Retorna la cantidad total de mamíferos creados
         return Mamifero.caballos + Mamifero.leones
+
+    # Métodos getters y setters
+    def isPelaje(self):
+        return self.pelaje
+
+    def setPelaje(self, pelaje):
+        self.pelaje = pelaje
+
+    def getPatas(self):
+        return self.patas
+
+    def setPatas(self, patas):
+        self.patas = patas
 
     @staticmethod
     def crearCaballo(nombre, edad, genero):
-        mamifero = Mamifero(nombre, edad, "pradera", genero)
-        Mamifero.caballos += 1
-        return mamifero
+        # Crear un Mamífero de tipo Caballo con valores predeterminados
+        caballo = Mamifero(nombre, edad, "pradera", genero, True, 4)
+        Mamifero.caballos += 1  # Incrementar el contador de caballos
+        return caballo
 
     @staticmethod
     def crearLeon(nombre, edad, genero):
-        mamifero = Mamifero(nombre, edad, "sabana", genero)
-        Mamifero.leones += 1
-        return mamifero
+        # Crear un Mamífero de tipo León con valores predeterminados
+        leon = Mamifero(nombre, edad, "selva", genero, True, 4)
+        Mamifero.leones += 1  # Incrementar el contador de leones
+        return leon
 
-    def toString(self):
-        return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi genero es {self.genero}"
