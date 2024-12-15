ed21ef9830c1c488759bd404783fdbb2652740e2
diana01010 <djacanamejoy@unal.edu.co>
2024-12-15 11:49:48 -0500

    animal

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -15,16 +15,13 @@ class Animal:
 
     @staticmethod
     def totalPorTipo():
-        return f"Mamíferos: {Mamifero.cantidadMamiferos()}\nAves: {Ave.cantidadAves()}\nReptiles: {Reptil.cantidadReptiles()}\nPeces: {Pez.cantidadPeces()}\nAnfibios: {Anfibio.cantidadAnfibios()}"
+        return f"Mamiferos : {Mamifero.cantidadMamiferos()}\nAves : {Ave.cantidadAves()}\nReptiles : {Reptil.cantidadReptiles()}\nPeces : {Pez.cantidadPeces()}\nAnfibios : {Anfibio.cantidadAnfibios()}"
 
     def __str__(self):
         info = f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi género es {self.genero}"
         if self.zona:
             info += f", la zona en la que me ubico es {self.zona.getNombre()}, en el {self.zona.getZoo().getNombre()}."
         return info
-
-    def movimiento(self):
-        return "desplazarse"
 
     # Métodos getters y setters
     def getNombre(self):
